a5cf517647550ccef16c8fab8be085b746c7d8b0
James Lotruglio <jimlotruglio@yahoo.com>
2018-09-18 20:40:58 +0000

    automatically backed up by learn

--- db/migrate/20180918203506_add_active_column.rb
+++ db/migrate/20180918203506_add_active_column.rb
@@ -1,5 +1,5 @@
 class AddActiveColumn < ActiveRecord::Migration
   def change
-    add_column :students, :boolean, :default=> false 
+    add_column :students, :active, :boolean, :default=> false 
   end
 end
